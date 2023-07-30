e13f9c3c9f75f3102fb5d3e163b3f6d688d2d46e
Viktoryia Varatnitskaya <vorotnic@Viktoryias-MacBook-Air.local>
2023-07-29 23:01:57 -0700

    added building blocks, openApi specification

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,25 +10,40 @@ import {
   Patch,
   Delete,
   Query,
+  UsePipes,
+  ValidationPipe,
+  SetMetadata,
+  ParseIntPipe,
 } from '@nestjs/common';
+import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { resourceLimits } from 'worker_threads';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Protocol } from '../common/decorators/protocol.decorator'
+import { ApiForbiddenResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeesService: CoffeesService) {}
+  constructor(private readonly coffeesService: CoffeesService) { }
 
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @Public()
   @Get()
-  findAll(@Query() paginationQuery: PaginationQueryDto) {
+  async findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto) {
     const { limit, offset } = paginationQuery;
+    // await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log(protocol)
     return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    console.log(id)
     console.log(typeof id);
     return this.coffeesService.findOne('' + id);
   }
@@ -40,7 +55,7 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: string, @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
+import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +19,17 @@ async function bootstrap() {
       },
     }),
   );
+
+  const options = new DocumentBuilder()
+  .setTitle('Iluvcoffee')
+  .setDescription('Coffee application')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+  
+  app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor())
   await app.listen(3000);
 }
 bootstrap();
